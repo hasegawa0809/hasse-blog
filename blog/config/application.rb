8668f6c8dd75492d2c852b4386dcd545cf5d4d12
長谷川大騎 <hasse0809@gmail.com>
2013-05-16 20:34:51 +0900

    erbをhamlに変更

--- blog/config/application.rb
+++ blog/config/application.rb
@@ -42,6 +42,12 @@ module Blog
     # Enable escaping HTML in JSON.
     config.active_support.escape_html_entities_in_json = true
 
+    config.generators do |g|
+     g.template_engine :haml
+     g.test_framework :rspec, :fixture => true, :views => false
+     g.fixture_replacement :factory_girl, :dir => "spec/factories"
+   end
+
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
